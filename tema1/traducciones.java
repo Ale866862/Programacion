dd611ba57f6f6a81176724500a86ba6ec12c0377
AleOjeda <sololausoparalasmurf.de.valo@gmail.com>
2026-09-22 12:11:12 +0200

    Add files via upload

---
+++ tema1/traducciones.java
@@ -0,0 +1,15 @@
+public class Traducciones {
+    public static void main(String[] args) {
+        System.out.println("English\tCastellano");
+        System.out.println("House\tCasa");
+        System.out.println("Dog\tPerro");
+        System.out.println("Cat\tGato");
+        System.out.println("Book\tLibro");
+        System.out.println("Table\tMesa");
+        System.out.println("Chair\tSilla");
+        System.out.println("Water\tAgua");
+        System.out.println("Food\tComida");
+        System.out.println("School\tEscuela");
+        System.out.println("Friend\tAmigo");
+    }
+}
