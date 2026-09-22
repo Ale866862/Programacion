12cdf5712523643c102efdeeaf68cf6111081502
AleOjeda <sololausoparalasmurf.de.valo@gmail.com>
2026-09-22 14:16:47 +0200

    Rename class and add new translation
    Changed class name from 'Traducciones' to 'traducciones' and added 'Wheel' translation.

--- tema1/traducciones.java
+++ tema1/traducciones.java
@@ -1,9 +1,9 @@
-public class Traducciones {
+public class traducciones {
     public static void main(String[] args) {
         System.out.println("English\tCastellano");
         System.out.println("Pencil\tLápiz");
         System.out.println("Dog\tPerro");
-        System.out.println("Keyboard\tTeclado");
+        System.out.println("Wheel\tRueda");
         System.out.println("Book\tLibro");
         System.out.println("Key\tLlave");
         System.out.println("Chair\tSilla");
