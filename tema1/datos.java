22f3d141bd5c09fb9e8587fc7360545501599ae8
AleOjeda <sololausoparalasmurf.de.valo@gmail.com>
2026-09-22 12:05:29 +0200

    Add files via upload

---
+++ tema1/datos.java
@@ -0,0 +1,8 @@
+//Este programa imprime mi nombre, mi dirección y mi número de teléfono en pantalla
+public class Datos {
+	public static void main(String[] args) {
+	System.out.println("Alejandro Ojeda Sánchez");
+	System.out.println("C/Antonio Machado 55");
+	System.out.println("+34 675 45 34 04");
+	}
+}
