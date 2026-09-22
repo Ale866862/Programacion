012ff58c7020332876ac2386a03e3a85d05d3cec
AleOjeda <sololausoparalasmurf.de.valo@gmail.com>
2026-09-22 14:15:20 +0200

    Update translations in Traducciones class

--- tema1/traducciones.java
+++ tema1/traducciones.java
@@ -1,15 +1,15 @@
 public class Traducciones {
     public static void main(String[] args) {
         System.out.println("English\tCastellano");
-        System.out.println("House\tCasa");
+        System.out.println("Pencil\tLápiz");
         System.out.println("Dog\tPerro");
-        System.out.println("Cat\tGato");
+        System.out.println("Keyboard\tTeclado");
         System.out.println("Book\tLibro");
-        System.out.println("Table\tMesa");
+        System.out.println("Key\tLlave");
         System.out.println("Chair\tSilla");
         System.out.println("Water\tAgua");
-        System.out.println("Food\tComida");
+        System.out.println("Glass\tCristal");
         System.out.println("School\tEscuela");
-        System.out.println("Friend\tAmigo");
+        System.out.println("Sand\tArena");
     }
 }
